5a890fbf7c9e4253bf4e0293f22ae67dec393245
HyhLovelycode <huangyihong0303@gmail.com>
2024-08-15 18:17:17 +0800

    add transform

--- data/defect.py
+++ data/defect.py
@@ -8,6 +8,80 @@ import PIL.Image as Image
 import numpy as np
 from torchvision import transforms
 from torch.utils.data import DataLoader
+import numbers
+import collections
+import random
+import cv2
+
+def calculate_channel_means(image: np.ndarray) -> list:
+    """
+    计算输入图像的三个通道的均值。
+
+    参数:
+        image (np.ndarray): 使用cv2读取的图像数据。
+
+    返回:
+        tuple: 三个通道的均值（B_mean, G_mean, R_mean）。
+    """
+    # 确保输入图像是三通道的
+    if len(image.shape) != 3 or image.shape[2] != 3:
+        raise ValueError("输入图像必须是三通道的（BGR）。")
+
+    # 计算每个通道的均值
+    b_mean = np.mean(image[:, :, 0])
+    g_mean = np.mean(image[:, :, 1])
+    r_mean = np.mean(image[:, :, 2])
+
+    return [b_mean, g_mean, r_mean]
+class RandRotate(object):
+    # Randomly rotate image & label with rotate factor in [rotate_min, rotate_max]
+    def __init__(self, rotate, padding, ignore_label=0, p=0.5):
+        assert (isinstance(rotate, collections.Iterable) and len(rotate) == 2)
+        if isinstance(rotate[0], numbers.Number) and isinstance(rotate[1], numbers.Number) and rotate[0] < rotate[1]:
+            self.rotate = rotate
+        else:
+            raise (RuntimeError("segtransform.RandRotate() scale param error.\n"))
+        assert padding is not None
+        assert isinstance(padding, list) and len(padding) == 3
+        if all(isinstance(i, numbers.Number) for i in padding):
+            self.padding = padding
+        else:
+            raise (RuntimeError("padding in RandRotate() should be a number list\n"))
+        assert isinstance(ignore_label, int)
+        self.ignore_label = ignore_label
+        self.p = p
+
+    def __call__(self, image, label):
+        if random.random() < self.p:
+            angle = self.rotate[0] + (self.rotate[1] - self.rotate[0]) * random.random()
+            h, w = label.shape
+            matrix = cv2.getRotationMatrix2D((w / 2, h / 2), angle, 1)
+            image = cv2.warpAffine(image, matrix, (w, h), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=self.padding)
+            label = cv2.warpAffine(label, matrix, (w, h), flags=cv2.INTER_NEAREST, borderMode=cv2.BORDER_CONSTANT, borderValue=self.ignore_label)
+        return image, label
+
+
+class RandomHorizontalFlip(object):
+    def __init__(self, p=0.5):
+        self.p = p
+
+    def __call__(self, image, label):
+        if random.random() < self.p:
+            image = cv2.flip(image, 1)
+            label = cv2.flip(label, 1)
+        return image, label
+
+
+class RandomVerticalFlip(object):
+    def __init__(self, p=0.5):
+        self.p = p
+
+    def __call__(self, image, label):
+        if random.random() < self.p:
+            image = cv2.flip(image, 0)
+            label = cv2.flip(label, 0)
+        return image, label
+
 
 
 class DatasetVISION(Dataset):
@@ -26,6 +100,7 @@ class DatasetVISION(Dataset):
         self.transform = transform
         self.class_ids = [ x for x in range(12)]
         self.img_metadata = self.build_img_metadata()
+        self.custom_transform = [RandRotate([0,360],[0,0,0]), RandomVerticalFlip(), RandomHorizontalFlip()]
 
     def __len__(self):
         return len(self.img_metadata)
@@ -42,8 +117,8 @@ class DatasetVISION(Dataset):
         if not self.use_original_imgsize:
             query_cmask = F.interpolate(query_cmask.unsqueeze(0).unsqueeze(0).float(), query_img.size()[-2:], mode='nearest').squeeze()
         query_mask = (query_cmask / 255).floor()
-       # if pair_type =="n":
-        #    query_mask = torch.zeros_like(query_mask)
+        if pair_type =="n":
+            query_mask = torch.zeros_like(query_mask)
         support_imgs = torch.stack([self.transform(support_img) for support_img in support_imgs])
 
         support_masks = []
@@ -79,21 +154,34 @@ class DatasetVISION(Dataset):
         query_mask = self.read_mask(query_name)
         support_imgs = [self.read_img(name) for name in support_names]
         support_masks = [self.read_mask(name) for name in support_names]
-
+        
         org_qry_imsize = query_img.size
+        if self.split == "train":
+            si = support_imgs[0]
+            sm = support_masks[0]
+            for t in self.custom_transform:
+                si,sm=t(si,sm)
+                query_img, query_mask = t(query_img, query_mask)
+            support_imgs = [si]
+            support_masks = [sm]
+        support_masks = [torch.tensor(i) for i in support_masks]
+        query_mask = torch.tensor(query_mask)
 
         return query_img, query_mask, support_imgs, support_masks, org_qry_imsize
 
 
     def read_mask(self, img_name):
         r"""Return segmentation mask in PIL Image"""
-        mask = torch.tensor(np.array(Image.open(os.path.join(self.ann_path, img_name) + '.png')))
+        mask = cv2.imread(os.path.join(self.ann_path, img_name) + '.png', cv2.IMREAD_GRAYSCALE)
+        # mask = torch.tensor(np.array(Image.open(os.path.join(self.ann_path, img_name) + '.png')))
         return mask
 
     def read_img(self, img_name):
         r"""Return RGB image in PIL Image"""
-        return Image.open(os.path.join(self.img_path, img_name) + '.jpg')
-
+        image = cv2.imread(os.path.join(self.img_path, img_name) + '.jpg')
+        image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+        # return Image.open(os.path.join(self.img_path, img_name) + '.jpg')
+        return image
     def sample_episode(self, idx):
         support_name, query_name, class_sample, pair_type = self.img_metadata[idx]
 
@@ -116,8 +204,6 @@ class DatasetVISION(Dataset):
             metadata = f.read().split('\n')[:-1]
         img_metadata=[]
         for data in metadata:
-            if data.split()[3]=="p":
-                pass
             img_metadata.append([data.split()[0], data.split()[1], int(data.split()[2]), data.split()[3]])
 
         print('Total (%s) images are : %d' % (self.split, len(img_metadata)))
@@ -134,9 +220,18 @@ class DatasetVISION(Dataset):
         return img_metadata_classwise
 
 if __name__ == "__main__":
-    datapath = "/home/hyh/Documents/Eccv/Dataset/VISION24-data-challenge-train/data"
-    tr=  transforms.Compose([transforms.ToTensor(), transforms.Resize(size=(800, 800))])
-    mydata = DatasetVISION(datapath,0,tr,"test",1,False)
+    datapath = "/home/hyh/Documents/Eccv/Dataset/VISION24-data-challenge-train/data_patch_1shot"
+    tr=  transforms.Compose([transforms.ToTensor(), transforms.Resize(size=(388, 388))])
+    mydata = DatasetVISION(datapath,0,tr,"train",1,False)
     dataloader = DataLoader(mydata, batch_size=4, shuffle=True, num_workers=1)
     for idx, batch in enumerate(dataloader):
         print(batch)
+    # image = Image.open("/home/hyh/Documents/Eccv/Dataset/VISION24-data-challenge-train/data_patch_1shot/images/Cable_thunderbolt_000000-block1.jpg")
+    # label = Image.open("/home/hyh/Documents/Eccv/Dataset/VISION24-data-challenge-train/data_patch_1shot/annotations/Cable_thunderbolt_000000-block1.png")
+    # pad = calculate_channel_means(image)
+    # cv2.imshow("o", image)
+    # R = RandRotate([0,360], pad )
+    # a,b = R(image,label)
+    # cv2.imshow("r", a)
+    # cv2.imshow("r_b", b)
+    # cv2.waitKey(0)
